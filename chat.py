617036b4c655692951ba351c0816769ee3a73f44
hbprotoss <gamespy1991@gmail.com>
2017-08-23 22:03:40 +0800

    接入 tuling123

--- chat.py
+++ chat.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
+import requests
+
 def onQQMessage(bot, contact, member, content):
-    print(contact, member, content)
-    if contact.ctype == 'group' and contact.name != '机器人测试':
-        return
+    # print(contact, member, content)
+    # if contact.ctype == 'group' and contact.name != '机器人测试':
+    #     return
 
     if content.startswith('[@ME]'):
         content = content[len('[@ME]'):]
@@ -23,14 +25,23 @@ def get_contact(bot, qq_id):
         return l[0]
 
 
+def get_response_from_tuling123(message, userid):
+    r = requests.post('http://www.tuling123.com/openapi/api', json={'key': 'a9ea09dc380c4e128d30f97386acc32f', 'info': message, 'userid': userid})
+    if r.status_code == 200:
+        response = r.json()
+        return response['text']
+    else:
+        return '啊，无法理解火星人的语言'
+
+
 def handle_common_message(bot, contact, member, content):
     pass
 
 
 def handle_my_message(bot, contact, member, content):
-    if ' ' not in content:
-        cmd = content
-        msg = ''
+    if not content.startswith('-'):
+        cmd = ''
+        msg = content
     else:
         cmd, msg = content.split(' ', 1)
 
@@ -43,3 +54,5 @@ def handle_my_message(bot, contact, member, content):
         if not person:
             return
         bot.SendTo(person, msg)
+    else:
+        bot.SendTo(contact, get_response_from_tuling123(msg, member.qq))
