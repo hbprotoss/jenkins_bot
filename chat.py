508c6fec51c0b034854a876608715c789c4f057f
hbprotoss <gamespy1991@gmail.com>
2017-08-24 20:39:41 +0800

    接入图灵接口所有功能

--- chat.py
+++ chat.py
@@ -2,6 +2,8 @@
 # coding=utf-8
 
 import requests
+from qqbot import utf8logger as logger
+
 
 def onQQMessage(bot, contact, member, content):
     # print(contact, member, content)
@@ -25,15 +27,6 @@ def get_contact(bot, qq_id):
         return l[0]
 
 
-def get_response_from_tuling123(message, userid):
-    r = requests.post('http://www.tuling123.com/openapi/api', json={'key': 'a9ea09dc380c4e128d30f97386acc32f', 'info': message, 'userid': userid})
-    if r.status_code == 200:
-        response = r.json()
-        return response['text']
-    else:
-        return '啊，无法理解火星人的语言'
-
-
 def handle_common_message(bot, contact, member, content):
     pass
 
@@ -55,4 +48,24 @@ def handle_my_message(bot, contact, member, content):
             return
         bot.SendTo(person, msg)
     else:
-        bot.SendTo(contact, get_response_from_tuling123(msg, member.qq))
+        handle_default_message(bot, contact, member, msg)
+
+
+def handle_default_message(bot, contact, member, content):
+    r = requests.post('http://www.tuling123.com/openapi/api',
+                      json={'key': 'a9ea09dc380c4e128d30f97386acc32f', 'info': content, 'userid': member.qq})
+    if r.status_code != 200:
+        logger.ERROR('data %s', r.json())
+        bot.SendTo(contact, '啊！人类无法理解的火星语')
+        return
+    response = r.json()
+    code = response['code']
+    msg = ''
+    if code == 100000:
+        msg = response['text']
+    elif code == 200000:
+        msg = response['text'] + " " + response['url']
+    elif code == 302000:
+        msg = response['text']
+        msg = msg + '\n\n'.join(('%s %s\n%s' % (n['article'], n['source'], n['detailurl']) for n in response['list']))
+    bot.SendTo(contact, msg)
